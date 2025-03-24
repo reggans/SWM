1eb0ca9915f461e9a3b5ec96758b8298e55c65e8
Faeyza Rishad <128669075+reggans@users.noreply.github.com>
2025-03-24 14:36:43 +0900

    -

--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 # python main.py --model unsloth/QwQ-32B-unsloth-bnb-4bit --model_source hf --max_tokens 2048 --runs 5 --n_boxes 8
 
 # LiteLLM
-# python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes 8
+python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes 8
 python main.py --model gemini/gemini-2.0-pro-exp-02-05 --model_source litellm --runs 5 --n_boxes 8
-# python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes 8
+python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes 8
 
-# python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes 8 --cot implicit
+python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes 8 --cot implicit
 python main.py --model gemini/gemini-2.0-pro-exp-02-05 --model_source litellm --runs 5 --n_boxes 8 --cot implicit
-# python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes 8 --cot implicit
+python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes 8 --cot implicit
 
-# python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes 8 --cot explicit
+python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes 8 --cot explicit
 python main.py --model gemini/gemini-2.0-pro-exp-02-05 --model_source litellm --runs 5 --n_boxes 8 --cot explicit
-# python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes 8 --cot explicit
+python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes 8 --cot explicit
