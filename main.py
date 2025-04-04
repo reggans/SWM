0fcb1fb171c8807cd3be78ca48ec699b4a949663
Faeyza Rishad <128669075+reggans@users.noreply.github.com>
2025-04-04 21:55:40 +0900

    add multiple tokens

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ Your final answer should be a number from 1-{n_boxes}, the index of the box you 
     # Start the test
     response = model.send_message(question)
     with tqdm(total=2*worst_case_n, desc="Total guesses") as guess_bar:
-        with tqdm(total=n_boxes * 2, desc="Token A") as token_bar:
+        with tqdm(total=n_boxes * 2, desc="Tokens") as token_bar:
             while (True):
                 token_box = [random.choice(legal_boxes[token]) for token in tokens]
                 opened_boxes = set()
@@ -126,6 +126,8 @@ Your final answer should be a number from 1-{n_boxes}, the index of the box you 
                             token_bar.update(1)
                             legal_boxes[token[i]].remove(token_box)
                             found_tokens.append(token[i])
+
+                            print(legal_boxes)
                     
                     msg = ""
                     if found:
