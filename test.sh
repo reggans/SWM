9bb21882a27c9eb37cc843206596c3998f0e1ce0
Faeyza Rishad <128669075+reggans@users.noreply.github.com>
2025-04-04 14:18:20 +0900

    more tests

--- test.sh
+++ test.sh
@@ -1,14 +1,23 @@
 # python main.py --model unsloth/QwQ-32B-unsloth-bnb-4bit --model_source hf --max_tokens 2048 --runs 5 --n_boxes 8
 
 # LiteLLM
-python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes 8
-python main.py --model gemini/gemini-2.0-pro-exp-02-05 --model_source litellm --runs 5 --n_boxes 8
-python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes 8
+# python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes 8
+python main.py --model gpt-4o-mini-2024-07-18 --model_source litellm --runs 5 --n_boxes 8
+python main.py --model gemini/gemini-2.0-flash --model_source litellm --runs 5 --n_boxes 8
+python main.py --model gemini/gemini-1.5-pro --model_source litellm --runs 5 --n_boxes 8
+# python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes 8
+python main.py --model anthropic/claude-3-5-haiku-latest --model_source litellm --runs 5 --n_boxes 8
 
-python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes 8 --cot implicit
-python main.py --model gemini/gemini-2.0-pro-exp-02-05 --model_source litellm --runs 5 --n_boxes 8 --cot implicit
-python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes 8 --cot implicit
+# python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes 8 --cot implicit
+python main.py --model gpt-4o-mini-2024-07-18 --model_source litellm --runs 5 --n_boxes 8 --cot implicit
+python main.py --model gemini/gemini-2.0-flash --model_source litellm --runs 5 --n_boxes 8 --cot implicit
+python main.py --model gemini/gemini-1.5-flash --model_source litellm --runs 5 --n_boxes 8 --cot implicit
+# python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes 8 --cot implicit
+python main.py --model anthropic/claude-3-5-haiku-latest --model_source litellm --runs 5 --n_boxes 8 --cot implicit
 
-python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes 8 --cot explicit
-python main.py --model gemini/gemini-2.0-pro-exp-02-05 --model_source litellm --runs 5 --n_boxes 8 --cot explicit
-python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes 8 --cot explicit
+# python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes 8 --cot explicit
+python main.py --model gpt-4o-mini-2024-07-18 --model_source litellm --runs 5 --n_boxes 8 --cot explicit
+python main.py --model gemini/gemini-2.0-flash --model_source litellm --runs 5 --n_boxes 8 --cot explicit
+python main.py --model gemini/gemini-1.5-pro --model_source litellm --runs 5 --n_boxes 8 --cot explicit
+# python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes 8 --cot explicit
+python main.py --model anthropic/claude-3-5-haiku-latest --model_source litellm --runs 5 --n_boxes 8 --cot explicit
