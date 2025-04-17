d9ea77f2e2c677f9b60eb0fd762b09e80346013b
Faeyza Rishad <128669075+reggans@users.noreply.github.com>
2025-04-17 16:20:48 +0900

    -

--- test.sh
+++ test.sh
@@ -6,24 +6,24 @@ do
     do
         # LiteLLM
         python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes $box --n_tokens $token
-        python main.py --model gpt-4o-mini-2024-07-18 --model_source litellm --runs 5 --n_boxes $box --n_tokens $token
+        # python main.py --model gpt-4o-mini-2024-07-18 --model_source litellm --runs 5 --n_boxes $box --n_tokens $token
         python main.py --model gemini/gemini-2.0-flash --model_source litellm --runs 5 --n_boxes $box --n_tokens $token
-        python main.py --model gemini/gemini-1.5-pro --model_source litellm --runs 5 --n_boxes $box --n_tokens $token
+        # python main.py --model gemini/gemini-1.5-pro --model_source litellm --runs 5 --n_boxes $box --n_tokens $token
         python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes $box --n_tokens $token
-        python main.py --model anthropic/claude-3-5-haiku-latest --model_source litellm --runs 5 --n_boxes $box --n_tokens $token
+        # python main.py --model anthropic/claude-3-5-haiku-latest --model_source litellm --runs 5 --n_boxes $box --n_tokens $token
 
         python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes $box --cot implicit --n_tokens $token
-        python main.py --model gpt-4o-mini-2024-07-18 --model_source litellm --runs 5 --n_boxes $box --cot implicit --n_tokens $token
+        # python main.py --model gpt-4o-mini-2024-07-18 --model_source litellm --runs 5 --n_boxes $box --cot implicit --n_tokens $token
         python main.py --model gemini/gemini-2.0-flash --model_source litellm --runs 5 --n_boxes $box --cot implicit --n_tokens $token
-        python main.py --model gemini/gemini-1.5-flash --model_source litellm --runs 5 --n_boxes $box --cot implicit --n_tokens $token
+        # python main.py --model gemini/gemini-1.5-flash --model_source litellm --runs 5 --n_boxes $box --cot implicit --n_tokens $token
         python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes $box --cot implicit --n_tokens $token
-        python main.py --model anthropic/claude-3-5-haiku-latest --model_source litellm --runs 5 --n_boxes $box --cot implicit --n_tokens $token
+        # python main.py --model anthropic/claude-3-5-haiku-latest --model_source litellm --runs 5 --n_boxes $box --cot implicit --n_tokens $token
 
         python main.py --model gpt-4.5-preview-2025-02-27 --model_source litellm --runs 5 --n_boxes $box --cot explicit --n_tokens $token
-        python main.py --model gpt-4o-mini-2024-07-18 --model_source litellm --runs 5 --n_boxes $box --cot explicit --n_tokens $token
+        # python main.py --model gpt-4o-mini-2024-07-18 --model_source litellm --runs 5 --n_boxes $box --cot explicit --n_tokens $token
         python main.py --model gemini/gemini-2.0-flash --model_source litellm --runs 5 --n_boxes $box --cot explicit --n_tokens $token
-        python main.py --model gemini/gemini-1.5-pro --model_source litellm --runs 5 --n_boxes $box --cot explicit --n_tokens $token
+        # python main.py --model gemini/gemini-1.5-pro --model_source litellm --runs 5 --n_boxes $box --cot explicit --n_tokens $token
         python main.py --model anthropic/claude-3-7-sonnet-latest --model_source litellm --runs 5 --n_boxes $box --cot explicit --n_tokens $token
-        python main.py --model anthropic/claude-3-5-haiku-latest --model_source litellm --runs 5 --n_boxes $box --cot explicit --n_tokens $token
+        # python main.py --model anthropic/claude-3-5-haiku-latest --model_source litellm --runs 5 --n_boxes $box --cot explicit --n_tokens $token
     done
 done
