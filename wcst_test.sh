2733a561b3099c71e8d7ea8bb3c327880a03d5fe
nadiarvi <nadia.arvi@gmail.com>
2025-05-11 23:38:31 +0900

    .

--- wcst_test.sh
+++ wcst_test.sh
@@ -3,4 +3,4 @@ do
     python wcst.py --model_source google --model gemini-2.0-flash --variant $variant --repeat 3
 
     python wcst.py --model_source hf --model unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit --variant $variant --repeat 3
-end
+done
