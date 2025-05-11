763b4cffe74fa26fcbb2db19c8b5a38afc28cb7d
nadiarvi <nadia.arvi@gmail.com>
2025-05-11 23:37:53 +0900

    .

---
+++ wcst_test.sh
@@ -0,0 +1,6 @@
+for variant in "card" "card-random" "string"
+do
+    python wcst.py --model_source google --model gemini-2.0-flash --variant $variant --repeat 3
+
+    python wcst.py --model_source hf --model unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit --variant $variant --repeat 3
+end
